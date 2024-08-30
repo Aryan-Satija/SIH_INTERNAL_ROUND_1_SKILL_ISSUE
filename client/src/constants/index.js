ba94b719cd72c35defc0e98e82b150fcebc3bc40
aryan <aryansatija2003@gmail.com>
2024-08-30 23:23:35 +0530

    added auction contract abi

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,4 @@
 import abi from './contract_abi.json';
 export const contractAbi = abi.abi;
-export const contractAddress = '0x4F8ea73946647cEb14434e8124d07E5FeD344c21'
+export const contractAddress = '0x4F8ea73946647cEb14434e8124d07E5FeD344c21';
+export const auctionContractAddress = '0xbc1084C25e2cf60be9CC9f7a115C707c163Cc5b6'; 

b2a2f8eb3e604a7e378f929232e3239b4e2d4517
aryan <aryansatija2003@gmail.com>
2024-08-31 19:39:06 +0530

    misc changes

--- client/src/services/pdf_apis.js
+++ client/src/services/pdf_apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL = process.env.SERVER_BASE_URL || `https://docsecure.onrender.com/api/v1`;
-const BASE_URL = 'http://localhost:8080/api/v1'
+const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL || `https://docsecure.onrender.com/api/v1`;
+// const BASE_URL = 'http://localhost:8080/api/v1'
 export const PDF_APIS = {
     encrypt_pdf_api : BASE_URL + `/pdf/encrypt-my-pdf`,
     encrypt_user_pdf_api: BASE_URL + `/pdf/encrypt-user-pdf`
